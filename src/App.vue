11ddc75ee7db87bc1f5c7e2dafb27d3c66ec07b9
IVAN NICOLAS ACERO <70918042+ivanAcero@users.noreply.github.com>
2021-09-08 20:48:18 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoaderComponent :visible="true" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LoaderComponent from "./components/loaderComponent/loaderComponent.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    LoaderComponent,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("./assets/global/normalize.scss");
 </style>
---
+++ src/components/loaderComponent/loaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="loaderComponent" v-if="visible">
+    <img src="../../assets/images/pokeball.svg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoaderComponent",
+  props: {
+    visible: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import url("loaderComponent.scss");
+</style>
+
